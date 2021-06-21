8ef0ea06a0f1261e0538a8497f23bc39d920e528
keane3192 <55885290+keane3192@users.noreply.github.com>
2021-06-21 11:43:00 +0300

    feat: added solution

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,18 @@
 package subtask2
 
 class Abbreviation {
-
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val reA = Regex("[a-zA-Z]+")
+        val reB = Regex("[A-Z]+")
+        if (reA.matches(a) && reB.matches(b)){
+            var achange = a.toLowerCase()
+            for(char in b.toLowerCase()){
+                if(achange.indexOf(char) != -1){
+                    achange = achange.substring(achange.indexOf(char) + 1)
+                } else return "NO"
+            }
+            return "YES"
+        } else return "NO"
     }
 }
