28bcd7638f6da1742d95ffdba1136f46427088a5
keane3192 <55885290+keane3192@users.noreply.github.com>
2021-06-21 11:43:52 +0300

    feat: added solution

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,35 @@
 package subtask3
-
+import java.time.LocalDate
 import kotlin.reflect.KClass
 
 class Blocks {
-
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var stringval = ""
+        var intval = 0
+        var dateval = LocalDate.parse("-999999999-01-01")
+        blockA.forEach { x ->
+            if (x != null) {
+                if (x::class == blockB && blockB == String()::class) {
+                    stringval += x
+                }
+                if (x::class == blockB && blockB == Int::class) {
+                    intval += x as Int
+                }
+                if (x::class == blockB && blockB == LocalDate::class) {
+                    val comp = x as LocalDate
+                    if (dateval < comp) {
+                        dateval = comp
+                    }
+                }
+            }
+        }
+        when(blockB) {
+            String()::class -> return stringval
+            Int::class -> return intval
+            LocalDate::class -> return "${dateval.dayOfMonth}.${dateval.monthValue}.${dateval.year}"
+            else -> return ""
+        }
     }
 }
+
